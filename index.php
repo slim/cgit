f2dccccebdd6a2b199003317f1749e3bd7bcefec
Slim Amamou <slim.amamou@gmail.com>
2008-04-18 12:02:35 +0200

    j'ai ajoute un log. maintenant cgit dump $_GET et $_POST dans un fichier cgit.log en format json

--- index.php
+++ index.php
@@ -3,5 +3,8 @@
 	foreach ($_GET as $command => $arguments) {
 		passthru("cd .. && git $command $arguments 2>&1");
 	}
+	$timeStamp = date('c');
+	$logEntry = "<code time='$timeStamp'>". json_encode($_GET) . json_encode($_POST) ."</code>";
+	file_put_contents('../cgit.log', $logEntry, FILE_APPEND);
 ?>
 </pre>
